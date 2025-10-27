9761e7867bdbbd67562864e9ed63cf434ddd3e9a
sandeep Kumar <sandeepkumarthakur9888@gmail.com>
2025-10-27 10:29:17 +0530

    Setup Node and All about File System

--- 02_node_fileSystem/index.js
+++ 02_node_fileSystem/index.js
@@ -29,3 +29,26 @@ const fs = require("fs");
 //     if(err) console.error(err);
 //     else console.log("Removed")
 // })
+
+// Remove Folder
+// fs.rm("./copyfiled", { recursive: true }, function (err) {
+//   if (err) console.error(err);
+//   else console.log("Remove Folder");
+// });
+
+// Read File
+// fs.readFile("./copyFile/hello.txt", "utf8", function (err, data) {
+//   if (err) console.error(err);
+//   console.log(data);
+// });
+
+// Create Folder
+// fs.mkdir("./createdFile/demo.txt", { recursive: false }, function (err) {
+//   if (err) console.error(err);
+//   else console.log("Folder created successfully!");
+// });
+
+// fs.writeFile("./createdFile/demo.txt", "This is Create by FS." , function (err) {
+//   if (err) console.error(err);
+//   else console.log("create file and Text Added Successful");
+// });
