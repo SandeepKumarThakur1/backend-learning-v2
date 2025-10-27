5f733aa53c1949ee57290fa78950a9d43d74123b
sandeep Kumar <sandeepkumarthakur9888@gmail.com>
2025-10-27 10:02:51 +0530

    Setup Node and All about File System

---
+++ 02_node_fileSystem/index.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+
+// Create File
+// fs.writeFile("demo.txt","Hey, this is demo data file with create text. ", function(err){
+//     if(err) console.error(err);
+//     else console.log("Done Task.")
+// })
+
+// Add text in File
+// fs.appendFile("demo.txt","Hey, I'm Sandeep Kumar.", function(err){
+//     if(err) console.error(err);
+//     else console.log("Done Task.")
+// })
+
+// Rename file
+// fs.rename("demo.txt","changeFile.txt",function(err){
+//     if(err) console.error(err)
+//     else console.log("Changed File Name.")
+// })
+
+// Copy File
+// fs.copyFile("changeFile.txt","./copyfiled/copy.txt",function(err){
+//     if(err) console.error(err);
+//     else console.log("Copied File");
+// })
+
+// Remove File
+// fs.unlink("changeFile.txt",function(err){
+//     if(err) console.error(err);
+//     else console.log("Removed")
+// })
